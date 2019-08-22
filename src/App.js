c37c29a1bf4a81168c41b915471e46ff9239e801
Martha Sharpe <marthasharpe2020@gmail.com>
2019-08-22 11:09:58 -0400

    Different Components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
 
+const Header = () => {
+  return (
+  <header className="App-header">
+    <h1>My Practice App!</h1>
+    <h2>An App just for practicing React</h2>
+  </header>
+  );
+}
+
+const Footer = () => {
+  return (
+  <footer className="App-footer">
+    <p>This is a link to my portfolio page:</p>
+    <a
+      className="App-link"
+      href="http://marthadawn.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      MarthaDawn.com
+    </a>
+  </footer>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          This will be my app!
-        </p>
-        <a
-          className="App-link"
-          href="http://marthadawn.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MarthaDawn.com
-        </a>
-      </header>
+      <Header />
+      
+      <Footer />
     </div>
   );
 }
