2b4869bb0250e524f1a14d9468c99a5ebfc7480e
Martha Sharpe <marthasharpe2020@gmail.com>
2019-08-22 11:20:25 -0400

    Current date component

--- src/App.js
+++ src/App.js
@@ -9,10 +9,19 @@ const Header = () => {
   );
 }
 
+const CurrentDate = (props) => {
+  return (
+  <div className="App-date">
+      <h3>Current date:</h3>
+      <p>{props.date}</p>
+  </div>
+  );
+}
+
 const Footer = () => {
   return (
   <footer className="App-footer">
-    <p>This is a link to my portfolio page:</p>
+    <h3>Portfolio page:</h3>
     <a
       className="App-link"
       href="http://marthadawn.com"
@@ -29,7 +38,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      
+      <CurrentDate date={Date()} />
       <Footer />
     </div>
   );
