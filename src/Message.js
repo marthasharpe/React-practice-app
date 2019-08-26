67ca91387307c71bbd7e931721b941ecd92915d8
Martha Sharpe <marthasharpe2020@gmail.com>
2019-08-26 12:31:39 -0400

    Check shouldComponentUpdate for Message

--- src/Message.js
+++ src/Message.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 class Dialog extends React.Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('Should I update?');
+        return nextProps.message !== this.props.message;
+      }
     componentWillUpdate() {
         alert('Component is about to update...');
     }
     componentWillReceiveProps(nextProps) {
-        console.log(this.props);
-        console.log(nextProps);
+        console.log(this.props, nextProps);
     }
     render() {
         return <h1>{this.props.message}</h1>
@@ -20,7 +23,7 @@ class Message extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          message: 'First Message'
+            message: 'First Message'
         };
         this.changeMessage = this.changeMessage.bind(this);
     }
